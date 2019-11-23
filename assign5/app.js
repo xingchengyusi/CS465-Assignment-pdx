0ad1b02befd040c25082699a100bf03b611d7398
xingchengyusi <zsxxxzks1314@gmail.com>
2019-11-23 00:01:55 -0800

    finish 5

--- assign5/app.js
+++ assign5/app.js
@@ -6,7 +6,7 @@ app.get('/submit', function(req, res, next) {
     let name = query.name;
     let email = query.email;
     let comment = query.comment;
-    let rep = '<p>name: ${name}</p>' + '<p>email: ${email}</p>' + '<p>comment: ${comment}</p>';
+    let rep = '<p>name: ' + name + '</p>' + '<p>email: ' + email + '</p>' + '<p>comment: ' + comment + '</p>';
     res.send(rep);
 
     res.end();
